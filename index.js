7fcfb43eb970b653a268a322f0b0b94b1ee4aea6
Mohan-8 <smohanakrishnan82@gmail.com>
2024-07-17 12:53:22 +0530

    updated

--- index.js
+++ index.js
@@ -7,18 +7,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/users/", require("./routes/userRoutes"));
-app.post("/api/users/signup", require("./routes/userRoutes"));
-app.get("/api/users/success", require("./routes/userRoutes"));
-app.get("/api/users/cancel", require("./routes/userRoutes"));
-app.post("/api/users/login", require("./routes/userRoutes"));
-app.get("/api/users/getUserDetails/:id", require("./routes/userRoutes"));
-app.put("/api/users/updateUserDetails/:id", require("./routes/userRoutes"));
-app.get("/api/users/getAllUserDetails", require("./routes/userRoutes"));
+app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
 
